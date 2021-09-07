5aea543f05ecce7deafab9bd087ac082c8aa80b9
Safa Nasir <49034994+safakhan413@users.noreply.github.com>
2021-09-07 00:51:33 -0400

    'x'

--- dictionary/settings.py
+++ dictionary/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '&e5z@dhmny9(zaf8us3%o(0vshnz@mdp)p6bqd*+cym^lkhqmo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [ 'dictionary1safa.herokuapp.com','127.0.0.1', 'localhost']
 # 'dictionary1safa.herokuapp.com'
