09c4e6a1c53d1de09ec8208b7bc9718138126d2c
Safa Nasir <49034994+safakhan413@users.noreply.github.com>
2021-09-09 00:59:20 -0400

    'd'

--- dictionary/settings.py
+++ dictionary/settings.py
@@ -157,4 +157,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
